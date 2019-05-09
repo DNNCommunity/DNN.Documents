530d7af155cbb0b98a3c6f52f81d306aac70927f
Rick Harris <rdh755@omnitecinc.com>
2019-05-09 14:45:20 -0700

    7.1.1 - Logic improvements
    Improved the method further to updating the logic and checking if any SuperAdmins were loaded.

--- Source/DnnDocuments/EditDocs.ascx.vb
+++ Source/DnnDocuments/EditDocs.ascx.vb
@@ -616,28 +616,34 @@ Namespace DotNetNuke.Modules.Documents
         Private Sub PopulateOwnerList()
             ' populate owner list
             Dim objUser As List(Of DotNetNuke.Entities.Users.UserInfo)
-            objUser = UserController.GetUsers(False, False, PortalId).Cast(Of UserInfo)().ToList()
+            objUser = UserController.GetUsers(False, False, PortalId).Cast(Of UserInfo)().Distinct().ToList()
 
-            'GetUsers doesn't return super-users, but they can own documents
-            Dim objSuperUser As List(Of DotNetNuke.Entities.Users.UserInfo)
-            objSuperUser = UserController.GetUsers(Null.NullInteger).Cast(Of UserInfo)().ToList()
-
-            'Compare the two lists and remove the duplicate super users from the users
-            For Each user As DotNetNuke.Entities.Users.UserInfo In objUser
-                For Each superuser As DotNetNuke.Entities.Users.UserInfo In objSuperUser
-                    If user.DisplayName = superuser.DisplayName Then
-                        objSuperUser.Remove(user)
-                        Exit For
-                    End If
-                Next
-            Next
-
-            'load data into the owner list
-            lstOwner.DataSource = objSuperUser.OrderBy(Function(i As UserInfo) i.DisplayName)
+            'load owners into the owner list
+            lstOwner.DataSource = objUser.OrderBy(Function(i As UserInfo) i.DisplayName)
             lstOwner.DataTextField = "DisplayName"
             lstOwner.DataValueField = "UserId"
 
             lstOwner.DataBind()
+
+            'GetUsers doesn't return super-users, but they can own documents
+            Dim objSuperUser As List(Of DotNetNuke.Entities.Users.UserInfo)
+            objSuperUser = UserController.GetUsers(Null.NullInteger).Cast(Of UserInfo)().Distinct().ToList()
+
+            'Compare the two lists and remove the duplicate super user
+            If objSuperUser.Any() Then
+                For Each user As DotNetNuke.Entities.Users.UserInfo In objUser
+                    For Each superuser As DotNetNuke.Entities.Users.UserInfo In objSuperUser
+                        If user.DisplayName = superuser.DisplayName Then
+                            lstOwner.Items.Remove(superuser.DisplayName)
+                            Exit For
+                        Else
+                            lstOwner.Items.Insert(0, New System.Web.UI.WebControls.ListItem(superuser.DisplayName, superuser.UserID.ToString))
+                            Exit For
+                        End If
+                    Next
+                Next
+            End If
+
 
             lstOwner.Items.Insert(0, New System.Web.UI.WebControls.ListItem(Services.Localization.Localization.GetString("None_Specified"), "-1"))
             '' End With
